0f7fd0aac4f068ddcd005df2a234196c1a7fc8ec
zhchplay <1947994772@qq.com>
2018-03-02 16:00:18 +0800

    cao

---
+++ w5zg-app/src/components/HelloWorld.vue
@@ -0,0 +1,79 @@
+<template>
+	<div class="hello show-grid">
+		<div class="row ">
+			<div class="col-md-1">.col-md-1</div>
+			<div class="col-md-1">.col-md-1</div>
+			<div class="col-md-1">.col-md-1</div>
+			<div class="col-md-1">.col-md-1</div>
+			<div class="col-md-1">.col-md-1</div>
+			<div class="col-md-1">.col-md-1</div>
+			<div class="col-md-1">.col-md-1</div>
+			<div class="col-md-1">.col-md-1</div>
+			<div class="col-md-1">.col-md-1</div>
+			<div class="col-md-1">.col-md-1</div>
+			<div class="col-md-1">.col-md-1</div>
+			<div class="col-md-1">.col-md-1</div>
+		</div>
+		<div class="row">
+			<div class="col-md-8">.col-md-8</div>
+			<div class="col-md-4">.col-md-4</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">.col-md-4</div>
+			<div class="col-md-4">.col-md-4</div>
+			<div class="col-md-4">.col-md-4</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">.col-md-6</div>
+			<div class="col-md-6">.col-md-6</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="input-group">
+					<span class="input-group-btn">
+		      		 	<button class="btn btn-default" type="button">-</button>
+		      		</span>
+					<input type="text" class="form-control" placeholder="2">
+					<span class="input-group-btn">
+		       			<button class="btn btn-default" type="button">+</button>
+		      		</span>
+				</div>
+			</div>
+		</div>
+		<form>
+			<div class="form-group">
+				<label for="exampleInputEmail1">Email address</label>
+				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+			</div>
+			<div class="form-group">
+				<label for="exampleInputPassword1">Password</label>
+				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+			</div>
+			<div class="form-group">
+				<label for="exampleInputFile">File input</label>
+				<input type="file" id="exampleInputFile">
+				<p class="help-block">Example block-level help text here.</p>
+			</div>
+			<div class="checkbox">
+				<label>
+      				<input type="checkbox"> Check me out
+    			</label>
+			</div>
+			<button type="submit" class="btn btn-default">Submit</button>
+		</form>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'HelloWorld',
+		data() {
+
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	@import url("../../static/css/HellowWorld.css");
+</style>
---
+++ w5zg-app/src/views/Home.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="container">
+	   <!-- <div class="row" >
+	      <div class="col-xs-6 col-sm-3" 
+	         style="background-color: #dedef8;
+	         box-shadow: inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
+	         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+	      </div>
+	      <div class="col-xs-6 col-sm-3" 
+	         style="background-color: #dedef8;box-shadow: 
+	         inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
+	         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+	            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+	            enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+	            nisi ut aliquip ex ea commodo consequat.
+	         </p>
+	         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+	            eiusmod tempor incididunt ut. 
+	         </p>
+	      </div>
+
+	      <div class="clearfix visible-xs"></div>
+
+	      <div class="col-xs-6 col-sm-3" 
+	         style="background-color: #dedef8;
+	         box-shadow:inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
+	         <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+	            laboris nisi ut aliquip ex ea commodo consequat. 
+	         </p>
+	      </div>
+	      <div class="col-xs-6 col-sm-3" 
+	         style="background-color: #dedef8;box-shadow: 
+	         inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
+	         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+	            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+	            enim ad minim 
+	         </p>
+	      </div>
+	   </div> -->
+	
+
+		<small>本行内容是在标签内</small><br>
+		<strong>本行内容是在标签内</strong><br>
+		<em>本行内容是在标签内，并呈现为斜体</em><br>
+		<p class="text-left">向左对齐文本</p>
+		<p class="text-center">居中对齐文本</p>
+		<p class="text-right">向右对齐文本</p>
+		<p class="text-muted">本行内容是减弱的</p>
+		<p class="text-primary">本行内容带有一个 primary class</p>
+		<p class="text-success">本行内容带有一个 success class</p>
+		<p class="text-info">本行内容带有一个 info class</p>
+		<p class="text-warning">本行内容带有一个 warning class</p>
+		<p class="text-danger">本行内容带有一个 danger class</p>
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style scoped>
+	
+</style>
